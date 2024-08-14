5cf9a60a0dcb2b3fa36b84f21d965a264bc482d7
pachipalasiva <120707070+pachipalasiva@users.noreply.github.com>
2024-08-14 07:23:06 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -2,6 +2,12 @@
 #include "typewise-alert.h"
 
 TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
+  // Test for inferBreach function
+  void test_inferBreach() {
+  TemperatureRange range = {0, 35};
 
-
+  assert(inferBreach(-1, range) == TOO_LOW);
+  assert(inferBreach(36, range) == TOO_HIGH);
+  assert(inferBreach(20, range) == NORMAL);
+}
 }
