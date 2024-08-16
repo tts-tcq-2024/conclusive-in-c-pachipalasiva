e42a772b669b6ef802ae4c50eeb3c8415e4ec14b
pachipalasiva <120707070+pachipalasiva@users.noreply.github.com>
2024-08-16 15:56:59 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -12,7 +12,7 @@ typedef enum {
   TOO_HIGH
 } BreachType;
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+BreachType inferBreach(double value, TemperatureRange range);
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 
 typedef enum {
