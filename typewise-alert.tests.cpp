24af319c4b8b4de011e939e75cf49d13092f7525
pachipalasiva <120707070+pachipalasiva@users.noreply.github.com>
2024-08-16 16:10:18 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -5,3 +5,14 @@ TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLow) {
   TemperatureRange range = {0, 35};  // lower limit 0, upper limit 35
   EXPECT_EQ(inferBreach(-1, range), TOO_LOW);
 }
+
+TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToHIGH) {
+  TemperatureRange range = {0, 35};  // lower limit 0, upper limit 36
+  EXPECT_EQ(inferBreach(36, range), TOO_HIGH);
+}
+
+TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToNormal) {
+  TemperatureRange range = {0, 35};  // lower limit 0, upper limit 36
+  EXPECT_EQ(inferBreach(20, range), NORMAL);
+}
+
